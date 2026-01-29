e40879c753991eab4ac92ea5f17799438969ae27
Sean LaPlante <laplante.sean@gmail.com>
2026-01-29 11:08:59 -0500

    Fixes for shellcheck and kernel config

--- scripts/debug-kvm-patch.sh
+++ scripts/debug-kvm-patch.sh
@@ -15,7 +15,7 @@ fi
 KERNEL_SRC=$1
 SIGNING_KEY_DIR=$2
 
-pushd "$KERNEL_SRC" || { echo "Failed to change directory to $KERNEL_SRC"; exit 1; }
+pushd "${KERNEL_SRC}" || { echo "Failed to change directory to ${KERNEL_SRC}"; exit 1; }
 
 echo "Building KVM kernel modules"
 make olddefconfig scripts prepare modules_prepare || { echo "Kernel preparation failed"; exit 1; }
@@ -25,8 +25,8 @@ echo "Signing"
 objcopy --remove-section .BTF ./arch/x86/kvm/kvm.ko
 objcopy --remove-section .BTF ./arch/x86/kvm/kvm-intel.ko
 
-./scripts/sign-file sha256 $SIGNING_KEY_DIR/MOK.priv $SIGNING_KEY_DIR/MOK.der ./arch/x86/kvm/kvm-intel.ko
-./scripts/sign-file sha256 $SIGNING_KEY_DIR/MOK.priv $SIGNING_KEY_DIR/MOK.der ./arch/x86/kvm/kvm.ko
+./scripts/sign-file sha256 "${SIGNING_KEY_DIR}/MOK.priv" "${SIGNING_KEY_DIR}/MOK.der" ./arch/x86/kvm/kvm-intel.ko
+./scripts/sign-file sha256 "${SIGNING_KEY_DIR}/MOK.priv" "${SIGNING_KEY_DIR}/MOK.der" ./arch/x86/kvm/kvm.ko
 
 sudo rmmod kvm_intel kvm || echo "Modules not loaded, skipping rmmod"
 sudo insmod ./arch/x86/kvm/kvm.ko || { echo "Failed to insert kvm.ko"; exit 1; }
