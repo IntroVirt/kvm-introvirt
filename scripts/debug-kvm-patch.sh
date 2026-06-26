fa39cb1f01d9b804d072aa8ae4f278263c32464f
Sean LaPlante <1337331+laplante-sean@users.noreply.github.com>
2026-06-26 11:02:54 -0400

    Patch for 6.17.0-35-generic (#27)
    * Patch for 6.17.0-35-generic
    * Removed old kernels from CI. We don't need to continue auto-building every single one. Added to patch debug script to toggle more verbose output

--- scripts/debug-kvm-patch.sh
+++ scripts/debug-kvm-patch.sh
@@ -13,14 +13,33 @@ if [[ $EUID -ne 0 ]]; then
     exit 1
 fi
 
+# Default to 0 if not set.
+# 0 = no tracing
+# 1 = trace hypercalls and invlpg events
+# 2 = trace single step and exception events (and above)
+# 3 = trace fast syscall and fast syscall return events (and above)
+_KVM_INTROVIRT_TRACE_EVENTS=${KVM_INTROVIRT_TRACE_EVENTS:=0}
+echo "KVM introspection trace events level: ${_KVM_INTROVIRT_TRACE_EVENTS}"
+
 KERNEL_SRC=$1
 SIGNING_KEY_DIR=$2
+
+if [[ ! -d "${KERNEL_SRC}" ]]; then
+    echo "Kernel source directory ${KERNEL_SRC} does not exist!"
+    exit 1
+fi
+
+if [[ ! -d "${SIGNING_KEY_DIR}" ]]; then
+    echo "Signing key directory ${SIGNING_KEY_DIR} does not exist!"
+    exit 1
+fi
 
 pushd "${KERNEL_SRC}" || { echo "Failed to change directory to ${KERNEL_SRC}"; exit 1; }
 
 echo "Building KVM kernel modules"
 make olddefconfig scripts prepare modules_prepare || { echo "Kernel preparation failed"; exit 1; }
-make M=arch/x86/kvm/ || { echo "KVM module build failed"; exit 1; }
+make M=arch/x86/kvm/ clean
+make M=arch/x86/kvm/ KCPPFLAGS="-DKVM_INTROVIRT_TRACE_EVENTS=${_KVM_INTROVIRT_TRACE_EVENTS}" || { echo "KVM module build failed"; exit 1; }
 
 echo "Signing"
 objcopy --remove-section .BTF ./arch/x86/kvm/kvm.ko
