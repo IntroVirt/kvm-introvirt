5e336ff587c7828f6a83b0796d4a9aadf343d332
Sean LaPlante <laplante.sean@gmail.com>
2026-06-27 09:33:39 -0400

    Adds AMD support to debugging script and removed requirment for signing key

--- scripts/debug-kvm-patch.sh
+++ scripts/debug-kvm-patch.sh
@@ -3,9 +3,17 @@
 # Helper to build and insert modified KVM kernel module for debugging
 # Run from the root of the repo
 
-if [[ $# -ne 2 ]]; then
-    echo "Usage: $0 <path-to-kernel-source> <path-to-signing-key-dir>"
+if [[ $# -ne 1 ]]; then
+    echo "Usage: $0 <path-to-kernel-source> [path-to-signing-key-dir]"
     exit 1
+fi
+
+KERNEL_SRC=$1
+
+if [[ $# -ne 2 ]]; then
+    SIGNING_KEY_DIR=""
+else
+    SIGNING_KEY_DIR=$2
 fi
 
 if [[ $EUID -ne 0 ]]; then
@@ -21,17 +29,23 @@ fi
 _KVM_INTROVIRT_TRACE_EVENTS=${KVM_INTROVIRT_TRACE_EVENTS:=0}
 echo "KVM introspection trace events level: ${_KVM_INTROVIRT_TRACE_EVENTS}"
 
-KERNEL_SRC=$1
-SIGNING_KEY_DIR=$2
-
 if [[ ! -d "${KERNEL_SRC}" ]]; then
     echo "Kernel source directory ${KERNEL_SRC} does not exist!"
     exit 1
 fi
 
-if [[ ! -d "${SIGNING_KEY_DIR}" ]]; then
+if [[ -n "${SIGNING_KEY_DIR}" && ! -d "${SIGNING_KEY_DIR}" ]]; then
     echo "Signing key directory ${SIGNING_KEY_DIR} does not exist!"
     exit 1
+else
+    echo "No signing key directory provided, will not sign the kernel modules"
+fi
+
+WHICH_MOD="kvm_intel"
+WHICH_KO="kvm-intel.ko"
+if grep -iq "amd" /proc/cpuinfo; then
+    WHICH_MOD="kvm_amd"
+    WHICH_KO="kvm-amd.ko"
 fi
 
 pushd "${KERNEL_SRC}" || { echo "Failed to change directory to ${KERNEL_SRC}"; exit 1; }
@@ -41,16 +55,20 @@ make olddefconfig scripts prepare modules_prepare || { echo "Kernel preparation 
 make M=arch/x86/kvm/ clean
 make M=arch/x86/kvm/ KCPPFLAGS="-DKVM_INTROVIRT_TRACE_EVENTS=${_KVM_INTROVIRT_TRACE_EVENTS}" || { echo "KVM module build failed"; exit 1; }
 
-echo "Signing"
-objcopy --remove-section .BTF ./arch/x86/kvm/kvm.ko
-objcopy --remove-section .BTF ./arch/x86/kvm/kvm-intel.ko
+if [[ -n "${SIGNING_KEY_DIR}" ]]; then
+    echo "Signing"
+    objcopy --remove-section .BTF ./arch/x86/kvm/kvm.ko
+    objcopy --remove-section .BTF ./arch/x86/kvm/kvm-intel.ko
+    objcopy --remove-section .BTF ./arch/x86/kvm/kvm-amd.ko
 
-./scripts/sign-file sha256 "${SIGNING_KEY_DIR}/MOK.priv" "${SIGNING_KEY_DIR}/MOK.der" ./arch/x86/kvm/kvm-intel.ko
-./scripts/sign-file sha256 "${SIGNING_KEY_DIR}/MOK.priv" "${SIGNING_KEY_DIR}/MOK.der" ./arch/x86/kvm/kvm.ko
+    ./scripts/sign-file sha256 "${SIGNING_KEY_DIR}/MOK.priv" "${SIGNING_KEY_DIR}/MOK.der" ./arch/x86/kvm/kvm-intel.ko
+    ./scripts/sign-file sha256 "${SIGNING_KEY_DIR}/MOK.priv" "${SIGNING_KEY_DIR}/MOK.der" ./arch/x86/kvm/kvm.ko
+    ./scripts/sign-file sha256 "${SIGNING_KEY_DIR}/MOK.priv" "${SIGNING_KEY_DIR}/MOK.der" ./arch/x86/kvm/kvm-amd.ko
+fi
 
-sudo rmmod kvm_intel kvm || echo "Modules not loaded, skipping rmmod"
+sudo rmmod ${WHICH_MOD} kvm || echo "Modules not loaded, skipping rmmod"
 sudo insmod ./arch/x86/kvm/kvm.ko || { echo "Failed to insert kvm.ko"; exit 1; }
-sudo insmod ./arch/x86/kvm/kvm-intel.ko || { echo "Failed to insert kvm-intel.ko"; exit 1; }
+sudo insmod ./arch/x86/kvm/${WHICH_KO} || { echo "Failed to insert ${WHICH_KO}"; exit 1; }
 popd || { echo "Failed to return to previous directory"; exit 1; }
 
 echo "KVM kernel modules inserted successfully"
